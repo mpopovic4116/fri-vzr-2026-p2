eb3d2d6c25f05c118584d3f76cb63ab595b748ec
Maks Popović <101988551+mpopovic4116@users.noreply.github.com>
2026-04-13 17:07:28 +0200

    add entrypoint for gif_comparator

--- src_python/vzr_p2/gif_comparator.py
+++ src_python/vzr_p2/gif_comparator.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 from PIL import Image, ImageChops
 
 
@@ -7,8 +9,7 @@ def cmp(img1, img2):
     frame = 0
     try:
         while True:
-            diff = ImageChops.difference(
-                img1.convert("RGB"), img2.convert("RGB"))
+            diff = ImageChops.difference(img1.convert("RGB"), img2.convert("RGB"))
             if diff.getbbox():
                 print(f"MISSMATCH AT FRAME: {frame}")
                 return False
@@ -16,9 +17,15 @@ def cmp(img1, img2):
             frame += 1
             img1.seek(img1.tell() + 1)
             img2.seek(img2.tell() + 1)
-    except Exception:
-        print(f"Gifs are identical.")
+    except Exception:  # noqa: BLE001
+        print("Gifs are identical.")
         return True
 
 
-cmp("lenia_cpu.gif", "lenia.gif")
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("img1")
+    parser.add_argument("img2")
+    parsed = parser.parse_args()
+
+    cmp(parsed.img1, parsed.img2)
